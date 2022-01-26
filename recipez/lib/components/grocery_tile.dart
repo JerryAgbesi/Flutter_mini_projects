8a207380331d15ff28a79f4b1c96e3eaffe41de8
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-01-26 00:54:53 -0800

    initial work on grocery tile

---
+++ recipez/lib/components/grocery_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroceryTile extends StatefulWidget {
+  const GroceryTile({Key? key}) : super(key: key);
+
+  @override
+  _GroceryTileState createState() => _GroceryTileState();
+}
+
+class _GroceryTileState extends State<GroceryTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- recipez/lib/screens/grocery_item_screen.dart
+++ recipez/lib/screens/grocery_item_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
@@ -80,7 +81,20 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
       body: Container(
         padding: const EdgeInsets.all(16.0),
         child: ListView(
-          children: [buildNameField()],
+          children: [
+            buildNameField(),
+            buildImportanceField(),
+            buildDateField(),
+            buildTimeField(),
+            const SizedBox(
+              height: 10,
+            ),
+            buildColorPicker(context),
+            const SizedBox(
+              height: 10,
+            ),
+            buildQuantityField(),
+          ],
         ),
       ),
     );
@@ -111,6 +125,205 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  Widget buildImportanceField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Importance',
+          style: GoogleFonts.lato(fontSize: 28),
+        ),
+        Wrap(
+          spacing: 10,
+          children: [
+            ChoiceChip(
+                selectedColor: Colors.black,
+                label: const Text(
+                  'Low',
+                  style: TextStyle(color: Colors.white),
+                ),
+                selected: _importance == Importance.low,
+                onSelected: (selected) {
+                  setState(() {
+                    _importance = Importance.low;
+                  });
+                }),
+            ChoiceChip(
+                selectedColor: Colors.black,
+                label: const Text(
+                  'Medium',
+                  style: TextStyle(color: Colors.white),
+                ),
+                selected: _importance == Importance.medium,
+                onSelected: (selected) {
+                  setState(() {
+                    _importance = Importance.medium;
+                  });
+                }),
+            ChoiceChip(
+                selectedColor: Colors.black,
+                label: const Text(
+                  'High',
+                  style: TextStyle(color: Colors.white),
+                ),
+                selected: _importance == Importance.high,
+                onSelected: (selected) {
+                  setState(() {
+                    _importance = Importance.high;
+                  });
+                }),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget buildDateField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Date',
+              style: TextStyle(fontSize: 28.0),
+            ),
+            TextButton(
+                onPressed: () async {
+                  final currentDate = DateTime.now();
+                  final selectedDate = await showDatePicker(
+                    context: context,
+                    initialDate: currentDate,
+                    firstDate: currentDate,
+                    lastDate: DateTime(currentDate.year + 50),
+                  );
+
+                  if (selectedDate != null) {
+                    setState(() {
+                      _dueDate = selectedDate;
+                    });
+                  }
+                },
+                child: Text('Select'))
+          ],
+        ),
+        Text('${DateFormat('dd-MM-yyy').format(_dueDate)}'),
+      ],
+    );
+  }
+
+  Widget buildTimeField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Time of Day',
+              style: TextStyle(fontSize: 28.0),
+            ),
+            TextButton(
+                onPressed: () async {
+                  final selectedTime = await showTimePicker(
+                      context: context, initialTime: TimeOfDay.now());
+
+                  if (selectedTime != null) {
+                    setState(() {
+                      _timeOfDay = selectedTime;
+                    });
+                  }
+                },
+                child: Text('Select'))
+          ],
+        ),
+        Text('${_timeOfDay.format(context)}'),
+      ],
+    );
+  }
+
+  Widget buildColorPicker(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 8.0),
+              height: 50,
+              width: 10,
+              color: _currentColor,
+            ),
+            Text(
+              'Color',
+              style: GoogleFonts.lato(fontSize: 28.0),
+            )
+          ],
+        ),
+        TextButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: BlockPicker(
+                      pickerColor: Colors.white,
+                      onColorChanged: (value) {
+                        setState(() {
+                          _currentColor = value;
+                        });
+                      },
+                    ),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('save'))
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text('Select'))
+      ],
+    );
+  }
+
+  Widget buildQuantityField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text('Quantity', style: GoogleFonts.lato(fontSize: 28)),
+            const SizedBox(
+              width: 8.0,
+            ),
+            Text(
+              _currentSlidervalue.toInt().toString(),
+              style: GoogleFonts.lato(fontSize: 18),
+            ),
+          ],
+        ),
+        Slider(
+            divisions: 100,
+            max: 100,
+            min: 0,
+            inactiveColor: _currentColor.withOpacity(0.5),
+            activeColor: _currentColor,
+            value: _currentSlidervalue.toDouble(),
+            label: _currentSlidervalue.toInt().toString(),
+            onChanged: (value) {
+              setState(() {
+                _currentSlidervalue = value.toInt();
+              });
+            })
       ],
     );
   }
