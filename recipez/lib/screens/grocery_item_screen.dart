965f72d3ff16e2e53ead442d113bcd73b8edea50
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-01-25 23:15:56 -0800

    continued work on the grocery item screen

--- recipez/lib/screens/grocery_item_screen.dart
+++ recipez/lib/screens/grocery_item_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:intl/number_symbols_data.dart';
 import 'package:uuid/uuid.dart';
 import 'package:recipez/models/models.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -13,8 +14,8 @@ class GroceryItemScreen extends StatefulWidget {
     required this.onCreate,
     required this.onUpdate,
     this.originalItem,
-  }) :isUpdating = (originalItem != null),
-   super(key: key);
+  })  : isUpdating = (originalItem != null),
+        super(key: key);
 
   final Function(GroceryItem) onCreate;
   final Function(GroceryItem) onUpdate;
@@ -26,8 +27,91 @@ class GroceryItemScreen extends StatefulWidget {
 }
 
 class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSlidervalue = 0;
+
+  @override
+  void initState() {
+    final originalItem = widget.originalItem;
+
+    if (originalItem != null) {
+      _nameController.text = originalItem.name;
+      _name = originalItem.name;
+      _currentSlidervalue = originalItem.quantity;
+      _importance = originalItem.importance;
+      _currentColor = originalItem.color;
+      final date = originalItem.date;
+      _timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+      _dueDate = date;
+    }
+
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.check)),
+        ],
+        elevation: 0.0,
+        title: Text(
+          'Grocery Item',
+          style: GoogleFonts.lato(fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [buildNameField()],
+        ),
+      ),
+    );
+  }
+
+  Widget buildNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Item name',
+          style: GoogleFonts.lato(fontSize: 23.0),
+        ),
+        TextField(
+          controller: _nameController,
+          cursorColor: _currentColor,
+          decoration: InputDecoration(
+            hintText: 'eg. Egg, 1 bag of rice, Apples',
+            border: UnderlineInputBorder(
+                borderSide: BorderSide(color: _currentColor)),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: _currentColor,
+              ),
+            ),
+            enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
