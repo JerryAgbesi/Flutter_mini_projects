09079268f2f6e2f26d824fb9ad2f207160d90f0e
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-01-31 20:49:36 -0800

    updated models.dart

---
+++ recipez/lib/models/app_state_manager.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:recipez/main.dart';
+
+class foodiezTab {
+  static const int explore = 0;
+  static const int recipes = 1;
+  static const int toBuy = 2;
+}
+
+class AppStateManager extends ChangeNotifier {
+  bool _initialized = false;
+  bool _loggedIn = false;
+  bool _onBoardingComplete = false;
+  int _selectedTab = foodiezTab.explore;
+
+  bool get isInitialized => _initialized;
+  bool get isloggedIn => _loggedIn;
+  bool get isonBoardingComplete => _onBoardingComplete;
+  int get getSelectedTab => _selectedTab;
+
+  void initializeApp() {
+    Timer(const Duration(milliseconds: 2000), () {
+      _initialized = true;
+
+      notifyListeners();
+    });
+  }
+
+  void login(String username, String password) {
+    _loggedIn = true;
+
+    notifyListeners();
+  }
+
+  void completeOnBoarding() {
+    _onBoardingComplete = true;
+
+    notifyListeners();
+  }
+
+  void gotoTab(index) {
+    _selectedTab = index;
+
+    notifyListeners();
+  }
+
+  void gotoRecipes() {
+    _selectedTab = foodiezTab.recipes;
+
+    notifyListeners();
+  }
+
+  void logout() {
+    _initialized = false;
+    _loggedIn = false;
+    _onBoardingComplete = false;
+    _selectedTab = 0;
+
+    initializeApp();
+    notifyListeners();
+  }
+}
