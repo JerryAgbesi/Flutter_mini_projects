a392a72bcd537852f3b3099cc7ab25e0359e2c61
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-02-21 23:07:55 -0800

    fixed few bugs

--- recipez_sp/lib/network/recipe_service.dart
+++ recipez_sp/lib/network/recipe_service.dart
@@ -1,1 +1,18 @@
 import 'package:http/http.dart';
+
+const String apiKey = '203109b6581a2e4a668f2d41fc02b6f3';
+const String apiId = 'ac2a9564';
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  Future getData(String url) async {
+    print('calling url:$url');
+    final response = await get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
