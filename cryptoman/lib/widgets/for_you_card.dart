4380d34fe8c1ac738785735b41bc53e1f3521a0a
JerryAgbesi <jerryeagbesi@gmail.com>
2022-10-07 14:56:53 +0000

    Add debit card list and navigation bar

---
+++ cryptoman/lib/widgets/for_you_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ForYouCard extends StatelessWidget {
+  const ForYouCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      width: 20,
+    );
+  }
+}
